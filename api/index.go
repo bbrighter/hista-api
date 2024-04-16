5f7b79e1abd220f899930e8326f4ce10cc565292
Benjamin Heller <benjamin.heller@mein.gmx>
2024-04-16 20:28:13 +0200

    allow CORS

--- api/index.go
+++ api/index.go
@@ -7,9 +7,27 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Request-Method", "*")
+		c.Writer.Header().Set("Access-Control-Content-Type", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, accept, origin, Cache-Control, If-None-Match, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PATCH, DELETE, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
 	// Creating new gin engine
 	g := gin.New()
+	g.Use(CORSMiddleware())
 	// Your handler logic
 	g.GET("/api/users", func(c *gin.Context) {
 		// ... your handler logic here ...
