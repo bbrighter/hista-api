49b683ad4874d778a38e8af41039241aef875ee4
Benjamin Heller <benjamin.heller@mein.gmx>
2024-04-17 20:31:08 +0200

    use db

--- api1/api1.go
+++ api1/api1.go
@@ -3,6 +3,9 @@ package api1
 import (
 	"context"
 
+	"encore.dev/beta/errs"
+	"encore.dev/storage/sqldb"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -11,11 +14,54 @@ type Service struct {
 	db *gorm.DB
 }
 
-type Response struct {
-	Resp string
+// Migrations disabled, get DB to run
+var blogDB = sqldb.NewDatabase("blog", sqldb.DatabaseConfig{
+	Migrations: "./migrations",
+})
+
+// initService initializes the site service.
+// It is automatically called by Encore on service startup.
+func initService() (*Service, error) {
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: blogDB.Stdlib(),
+	}))
+	if err != nil {
+		return nil, err
+	}
+	return &Service{db: db}, nil
+}
+
+type Version struct {
+	Resp    string
+	Version string
 }
 
 //encore:api public method=GET path=/test
-func Get(ctx context.Context) (Response, error) {
-	return Response{Resp: "Hallo Julia, ich bin eine API, aber noch keine Datenbank"}, nil
+func (s *Service) Get(ctx context.Context) (TodoItems, error) {
+	var todoItems []TodoItem
+	err := s.db.First(&todoItems).Error
+	return TodoItems{todoItems}, err
+}
+
+type TodoItem struct {
+	ID    int64  `gorm:"primaryKey" json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+}
+
+type TodoItems struct {
+	Items []TodoItem `json:"items"`
+}
+
+type PostParams struct {
+	Title string `json:"title"`
+}
+
+//encore:api public method=POST path=/test
+func (s *Service) Post(ctx context.Context, params PostParams) error {
+	if params.Title == "" {
+		return &errs.Error{Code: errs.InvalidArgument, Message: "Name needed"}
+	}
+	var todoItem = TodoItem{Title: params.Title, Done: false}
+	return s.db.Create(&todoItem).Error
 }
---
+++ api1/migrations/1_initial_setup.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE todo_items (
+    id BIGSERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    done BOOLEAN NOT NULL DEFAULT false
+);
