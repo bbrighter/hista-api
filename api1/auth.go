f699b2e27f905b0001f1431bc77075041b54c624
Benjamin Heller <benjamin.heller@mein.gmx>
2024-04-19 21:46:44 +0200

    feat: generate token and use user name

--- api1/auth.go
+++ api1/auth.go
@@ -9,27 +9,37 @@ import (
 )
 
 type AuthParams struct {
+	User     string
 	Password string
 }
 
 type Token struct {
-	Token *string
+	User   string
+	Bearer string
 }
 
-var currentToken Token
+var currentToken *Token
+
+func initToken() {
+	if currentToken == nil {
+		currentToken = &Token{Bearer: ""}
+	}
+	sb := strings.Builder{}
+	sb.Grow(50)
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	for i := 0; i < 50; i++ {
+		sb.WriteByte(charset[rand.Intn(len(charset))])
+	}
+	var tok string = sb.String()
+	currentToken.Bearer = tok
+	currentToken.User = "Julia"
+}
 
 //encore:api public method=POST path=/auth
-func (s *Service) Auth(ctx context.Context, params AuthParams) (Token, error) {
+func (s *Service) Auth(ctx context.Context, params AuthParams) (*Token, error) {
 	pw := params.Password
-	if pw == "123pi" {
-		sb := strings.Builder{}
-		sb.Grow(50)
-		const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		for i := 0; i < 50; i++ {
-			sb.WriteByte(charset[rand.Intn(len(charset))])
-		}
-		var tok string = sb.String()
-		currentToken.Token = &tok
+	if pw == "123pi" && params.User == "Julia" {
+		initToken()
 		return currentToken, nil
 	}
 	return currentToken, &errs.Error{Code: errs.Unauthenticated}
