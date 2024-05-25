753fbdf582471bd6e1afb91203a555bafaba6770
Benjamin Heller <benjamin.heller@mein.gmx>
2024-05-25 10:51:05 +0200

    fix: improve endpoint and make tests more stable

--- statistics/fixtures.sql
+++ statistics/fixtures.sql
@@ -1,21 +1,24 @@
 INSERT INTO "meals" ("date", "id") 
 VALUES ('2024-05-22 12:00:00.000', 1)
 ON CONFLICT ("id")
-DO NOTHING;
+DO UPDATE SET
+    "date"='2024-05-22 12:00:00.000';
 
 SELECT setval('meals_id_seq', 2, true);
 
 INSERT INTO "ingredients" ("name", "id")
 VALUES ('Name',1)
 ON CONFLICT ("id")
-DO NOTHING;
+DO UPDATE SET 
+    "name" = 'Name';
 
 SELECT setval('ingredients_id_seq', 2, true);
 
 INSERT INTO "foods" ("ingredient_id","condition","meal_id", "id") 
 VALUES (1,'cooked',1,1)
 ON CONFLICT ("id") 
-DO NOTHING;
+DO UPDATE SET 
+    "condition" = 'cooked';
 
 SELECT setval('foods_id_seq', 2, true);
 
--- statistics/statistic.go
+++ statistics/statistic.go
@@ -26,20 +26,31 @@ type Result struct {
 
 func getFoodsAndSymptoms(service *Service, fromDate time.Time, toDate time.Time, symptomIds []uint) ([]Result, error) {
 	var result []Result
-	var err error = service.db.Debug().
-		Select(
-			"foods.ingredient_id as ingredient_id",
-			"foods.condition as food_condition",
-			"SUM(CASE WHEN condition_events.date BETWEEN meals.date  AND meals.date + interval '72 hour' THEN 1 ELSE 0 END) as hours72",
-			"SUM(CASE WHEN condition_events.date BETWEEN meals.date  AND meals.date + interval '24 hour' THEN 1 ELSE 0 END) as hours24",
-			"SUM(CASE WHEN condition_events.date BETWEEN meals.date  AND meals.date + interval '1 hour' THEN 1 ELSE 0 END) as hours1",
-		).
+	subquery := service.db.Select(
+		"foods.ingredient_id as ingredient_id",
+		"foods.condition as food_condition",
+		"meals.id as meal_id",
+		"MAX(CASE WHEN condition_events.date BETWEEN meals.date  AND meals.date + interval '72 hour' THEN 1 ELSE 0 END) as hours72",
+		"MAX(CASE WHEN condition_events.date BETWEEN meals.date  AND meals.date + interval '24 hour' THEN 1 ELSE 0 END) as hours24",
+		"MAX(CASE WHEN condition_events.date BETWEEN meals.date  AND meals.date + interval '1 hour' THEN 1 ELSE 0 END) as hours1",
+	).
 		Table("meals").
 		Joins("JOIN foods ON meals.id = foods.meal_id").
 		Joins("JOIN condition_events ON condition_events.date BETWEEN meals.date  AND meals.date + interval '72 hour'").
 		Joins("JOIN conditions ON conditions.condition_event_id = condition_events.id").
 		Where("conditions.symptom_id in (?)", symptomIds).
 		Where("meals.date BETWEEN ? AND ?", fromDate, toDate).
+		Group("ingredient_id, food_condition, meals.id")
+
+	var err error = service.db.Debug().
+		Table("(?) as u", subquery).
+		Select(
+			"u.ingredient_id as ingredient_id",
+			"u.food_condition as food_condition",
+			"SUM(u.hours72) as hours72",
+			"SUM(u.hours24) as hours24",
+			"SUM(u.hours1) as hours1",
+		).
 		Group("ingredient_id, food_condition").
 		Scan(&result).Error
 
