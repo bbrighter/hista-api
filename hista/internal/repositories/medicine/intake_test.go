7abfe4e7f993cc4032cec6856cde9910253f4ad6
Benjamin Heller <benjamin.heller@mein.gmx>
2026-03-01 19:17:53 +0100

    feat: Return archived intakes

--- hista/internal/repositories/medicine/intake_test.go
+++ hista/internal/repositories/medicine/intake_test.go
@@ -10,20 +10,25 @@ import (
 func (s *MedicineRepoTestSuite) TestListGrouped() {
 	tests := map[string]struct {
 		numberOfIntakes   map[uint]int
+		useOld            bool
 		expectedLength    int
 		countForMedicine1 int64
 	}{
-		"empty list":         {},
-		"1 intake, med1":     {numberOfIntakes: map[uint]int{s.medicine1Id: 1}, expectedLength: 1, countForMedicine1: 1},
-		"2 intakes, med1":    {numberOfIntakes: map[uint]int{s.medicine1Id: 2}, expectedLength: 1, countForMedicine1: 2},
-		"archived not shown": {numberOfIntakes: map[uint]int{s.medicine1Id: 1, s.medicine2Id: 1, s.medicineArchivedId: 1}, expectedLength: 2, countForMedicine1: 1},
+		"empty list":      {},
+		"1 intake, med1":  {numberOfIntakes: map[uint]int{s.medicine1Id: 1}, expectedLength: 1, countForMedicine1: 1},
+		"2 intakes, med1": {numberOfIntakes: map[uint]int{s.medicine1Id: 2}, expectedLength: 1, countForMedicine1: 2},
+		"old not shown":   {numberOfIntakes: map[uint]int{s.medicine1Id: 1}, expectedLength: 0, countForMedicine1: 0, useOld: true},
 	}
 	for name, test := range tests {
 		s.Run(name, func() {
 			var intakes []entity.Intake
 			for medicineId, count := range test.numberOfIntakes {
+				var date = time.Now()
+				if test.useOld {
+					date = time.Now().Add(-24 * 14 * time.Hour)
+				}
 				for range count {
-					intakes = append(intakes, entity.Intake{PIID: GUID, Date: time.Now(), MedicineID: medicineId, MedicinePIID: GUID})
+					intakes = append(intakes, entity.Intake{PIID: GUID, Date: date, MedicineID: medicineId, MedicinePIID: GUID})
 				}
 			}
 			err := s.tx.CreateInBatches(intakes, 10).Error
