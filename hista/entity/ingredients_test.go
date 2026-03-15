37879f70f577992e3af89972f7fd7547248a0bb9
Benjamin Heller <benjamin.heller@mein.gmx>
2026-03-15 16:49:14 +0100

    fix test

--- hista/entity/ingredients_test.go
+++ hista/entity/ingredients_test.go
@@ -7,10 +7,11 @@ import (
 )
 
 func TestToIngredientResponse(t *testing.T) {
+	t.Parallel()
 	var prot int = 100
 	var carb int = 20
 	var fat int = 15
-	t.Parallel()
+	var fiber int = 0
 	var ingredient = Ingredient{
 		ID:         1,
 		Name:       "Name",
@@ -19,16 +20,17 @@ func TestToIngredientResponse(t *testing.T) {
 			Protein:      &prot,
 			Carbohydrate: &carb,
 			Fat:          &fat,
+			Fiber:        &fiber,
 		},
 	}
 	var resp IngredientResponse = ingredient.ToIngredientResponse()
 	assert.EqualValues(t, 1, resp.ID)
 	assert.Equal(t, "Name", resp.Name)
 	assert.Equal(t, true, resp.IsArchived)
-	assert.Equal(t, &prot, resp.Nutrition.Protein)
-	assert.Equal(t, &carb, resp.Nutrition.Carbohydrate)
-	assert.Equal(t, &fat, resp.Nutrition.Fat)
-	assert.Nil(t, resp.Nutrition.Fiber)
+	assert.Equal(t, prot, resp.Nutrition.Protein)
+	assert.Equal(t, carb, resp.Nutrition.Carbohydrate)
+	assert.Equal(t, fat, resp.Nutrition.Fat)
+	assert.Equal(t, fiber, resp.Nutrition.Fiber)
 
 }
 
